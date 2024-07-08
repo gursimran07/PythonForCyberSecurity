d2f4d2085b718d45359c2e92601000c4ebe2afb6
gursimran07 <gursimranlubana007@gmail.com>
2024-07-07 20:10:03 -0700

    demo programs

---
+++ Practice /7-7/calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 
+
+num1 = float(input("Enter the first number\n"))
+num2= float(input("Enter the second number\n"))
+activity= input("Enter the operator\n")
+
+if activity == "+":
+    print(num1+num2)
+if activity == "-":
+    print(num1-num2)
+if activity == "/":
+    print(num1/num2)
+if activity == "*":
+    print(num1*num2)
+
---
+++ Practice /7-7/helloWorldInput.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python 
+
+your_name = input("What is your name\n")
+
+print("Hello {0}...".format(your_name) + " How you doin?")
